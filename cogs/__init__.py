0ed798a768d053a69b4a4739e8b8c0a1557afbcb
Lirtico <davidestrella773@gmail.com>
2025-09-23 19:45:04 -0600

    help 3

---
+++ cogs/__init__.py
@@ -0,0 +1,2 @@
+# Cogs package initialization
+# This file makes the cogs directory a proper Python package
---
+++ events/__init__.py
@@ -0,0 +1,2 @@
+# Events package initialization
+# This file makes the events directory a proper Python package
